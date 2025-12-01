cff50a86a35ea73bd8f2d1420600998d83414b4b
JohanelENP <johanel.castillo@unl.edu.ec>
2025-11-30 19:07:57 -0500

    Actualización view con las correcciones finales

--- src/main/java/soccerSystem/view/RegistrarJugadoresExecute.java
+++ src/main/java/soccerSystem/view/RegistrarJugadoresExecute.java
@@ -2,14 +2,73 @@ package soccerSystem.view;
 
 import soccerSystem.domain.Equipo;
 import soccerSystem.domain.Jugador;
+import soccerSystem.domain.Persona;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class RegistrarJugadoresExecute {
 
-    public static void main(String[] args) {
 
-        Equipo equipo1 = new Equipo("Barcelona", "Visitante", "Rojo");
-        Jugador jugador = new Jugador("Alex", "Bravo", "1900337080", 8);
-        equipo1.agregarJugador(jugador);
-        equipo1.mostrarJugadores();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Nombre del equipo: ");
+        String nombreEquipo = sc.nextLine();
+        System.out.println("Denominacion del equipo: ");
+        String idEquipo = sc.nextLine();
+        System.out.println("Color del uniforme");
+        String colorUniforme = sc.nextLine();
+        Equipo equipo = new Equipo(nombreEquipo, idEquipo, colorUniforme);
+
+        boolean continuar = true;
+        while (continuar) {
+            while (continuar) {
+                System.out.println("\n REGISTRO DE JUGADORES");
+                System.out.println("Nombre del jugador: ");
+                String nombre = sc.nextLine();
+                System.out.println("Apellido del jugador: ");
+                String apellido = sc.nextLine();
+                String cedula = "";
+                boolean cedulaValidada = false;
+                while (!cedulaValidada) {
+                    try {
+                        System.out.println("Cédula del jugador: ");
+                        cedula = sc.nextLine();
+                        Persona.validarCedula(cedula);
+                        cedulaValidada = true;
+                    } catch (IllegalArgumentException e) {
+                        System.out.println("ERROR: " + e.getMessage());
+                        System.out.println("Intente nuevamente...\n");
+                    }
+                }
+                int camisa = 0;
+                boolean camisaValida = false;
+                while (!camisaValida) {
+                    try {
+                        System.out.println("Número de camisa del jugador: ");
+                        camisa = sc.nextInt();
+                        sc.nextLine();
+                        Jugador.validarNumeroCamisa(camisa);
+                        camisaValida = true;
+                    } catch (IllegalArgumentException e) {
+                        System.out.println("ERROR: " + e.getMessage());
+                        System.out.println("Intente nuevamente...\n");
+                    } catch (InputMismatchException e) {
+                        System.out.println("ERROR: Debe ingresar SOLO números.");
+                        sc.nextLine();
+                    }
+                }
+                Jugador jugador = new Jugador(nombre, apellido, cedula, camisa);
+                equipo.agregarJugador(jugador);
+
+                System.out.print("\n¿Desea registrar otro jugador? (si/no): ");
+                String opcion = sc.nextLine();
+                if (opcion.equalsIgnoreCase("no")) {
+                    continuar = false;
+                }
+            }
+            equipo.mostrarJugadores();
+        }
     }
 }
--- src/main/java/soccerSystem/view/RegistroSancionExecute.java
+++ src/main/java/soccerSystem/view/RegistroSancionExecute.java
@@ -9,19 +9,6 @@ import java.util.Scanner;
 
 public class RegistroSancionExecute {
 
-    public static void validarCedula(String cedula) {
-        if(cedula == null || cedula.length() != 10) {
-            throw new IllegalArgumentException("El cedula debe tener 10 digitos");
-        }
-    }
-    public static void validarNumeroCamisa(int numeroCamisa) {
-        if (numeroCamisa < 0 || numeroCamisa > 99) {
-            throw new IllegalArgumentException("El numero de camisa no es valido");
-        }
-        numeroCamisa = numeroCamisa;
-    }
-
-
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -39,10 +26,10 @@ public class RegistroSancionExecute {
             try {
                 System.out.println("Cedula del jugador: ");
                 cedula = sc.nextLine();
-                validarCedula(cedula);
+                Persona.validarCedula(cedula);
                 cedulaValidada = true;
             } catch (IllegalArgumentException aei) {
-                System.out.println("Ha ocurrido un Error " + aei.getMessage());
+                System.out.println("Ha ocurrido un Error " +"\n"+ aei.getMessage() );
                 System.out.println("Intente nuevamente");
             }
         }
@@ -53,10 +40,10 @@ public class RegistroSancionExecute {
             try {
                 System.out.println("Numero de Camisa del Jugador: ");
                 camisa = sc.nextInt();
-                validarNumeroCamisa(camisa);
+                Jugador.validarNumeroCamisa(camisa);
                 camisaValida = true;
             } catch (IllegalArgumentException aie) {
-                System.out.println("Ha ocurrido un Error" + aie.getMessage());
+                System.out.println("Ha ocurrido un Error" +"\n"+ aie.getMessage());
                 System.out.println("Intente nuevamente");
             } catch (InputMismatchException ea) {
                 System.out.println("ERROR: Debe ingresar SOLO números");
@@ -65,7 +52,7 @@ public class RegistroSancionExecute {
         }
         Jugador jugador = new Jugador(nombre,apellido,cedula,camisa);
         Sancion sancion = new Sancion(45, camisa,"FALTA ANTIDEPORTIVA",
-                encuentro,TipoDeSancion.TARJETA_ROJA);
+                encuentro,TipoDeSancion.TARJETA_ROJA, false);
 
         jugador.registrarSancion(sancion);
     }
